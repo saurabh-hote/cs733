dd6ba1031cdcdad39a557994ca50ef1d8591bb60
Saurabh Hote <saurabhh@cse.iitb.ac.in>
2015-02-10 04:47:35 +0530

    Small modification in the test case

--- raft_test.go
+++ raft_test.go
@@ -37,8 +37,7 @@ func init() {
 	data, _ := json.Marshal(*clusterConfig)
 	ioutil.WriteFile("config.json", data, 0644)
 
-//	serverReplicas := len((*clusterConfig).Servers)
-	serverReplicas := 2
+	serverReplicas := len((*clusterConfig).Servers)
 	programName := "server.go"
 	index := 1
 	for index <= serverReplicas {
